9571ad183e59df096e3b64ffbc147f6072f61bcb
shubhi@221199 <shubhisharma4u@gmail.com>
2024-01-21 22:07:59 +0530

    changes

--- Back-End/middleware/requestLoggerMiddleware.js
+++ Back-End/middleware/requestLoggerMiddleware.js
@@ -1,8 +1,7 @@
 const requestLoggerMiddleware = (req, res, next) => {
     // Log information about the incoming request
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-  
-    // Continue to the next middleware or route handler
+ 
     next();
   };
   
--- Back-End/routes/contactRoutes.js
+++ Back-End/routes/contactRoutes.js
@@ -13,8 +13,4 @@ router.route("/").get(getContacts).post(createContact);
 
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
-// router.route("/").post(createContact);
-// router.route("/:id").put(updateContact);
-// router.route("/:id").delete(deleteContact);
-
 module.exports = router;
